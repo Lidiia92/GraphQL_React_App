1e264416ad6fed101466e610e0c970884838a464
Lidii <lidiiany92@gmail.com>
2019-09-11 19:43:01 -0700

    updateUser mutation

--- schema/schema.js
+++ schema/schema.js
@@ -83,6 +83,20 @@ const mutation = new GraphQLObjectType({
                 return axios.delete(`http://localhost:3000/users/${args.id}`).
                 then(response => response.data)
             }
+        },
+        updateUser: {
+            type: UserType,
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLString)},
+                firstName: {type: GraphQLString},
+                age: {type: GraphQLInt},
+                companyId: {type: GraphQLString}
+            },
+            resolve(parentValue, args) {
+                return axios.patch(`http://localhost:3000/users/${args.id}`, {
+                    ...args
+                }).then(response => response.data)
+            }
         }
     }
 })
